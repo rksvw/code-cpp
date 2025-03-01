a1aa921266595940ec0d513be6a961bdf78a2360
rksvw <ritik.vw98@gmail.com>
2025-03-01 19:23:02 +0530

    Circular linked list some other methods

--- dataStructureAlgo/CircularList.cpp
+++ dataStructureAlgo/CircularList.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-#include<unistd.h>
-#include<cstdlib>
-#include<signal.h>
+#include <unistd.h>
+#include <cstdlib>
+#include <signal.h>
 using namespace std;
 
-void signal_callback_handler(int signum) {
+void signal_callback_handler(int signum)
+{
     cout << "->NULL" << signum << endl;
     // Terminate program
     exit(signum);
@@ -71,18 +72,87 @@ public:
         return;
     }
 
-    void print() {
-        if (head == NULL) return;
+    void print()
+    {
+        if (head == NULL)
+            return;
 
         cout << head->data << "->";
-        Node* temp = head->next;
+        Node *temp = head->next;
 
-        while(temp != head) {
+        while (temp != head)
+        {
             cout << temp->data << "->";
             temp = temp->next;
         }
+        cout << temp->data << endl;
+    }
 
-        cout << temp->data << "->NULL" << endl;
+    void deleteAtHead()
+    {
+        Node *temp = head;
+        if (head == NULL)
+        {
+            return;
+        }
+        else if (head == tail)
+        {
+            head = tail = NULL;
+            return;
+        }
+        else
+        {
+
+            head = temp->next;
+            tail->next = head;
+            tail->next = NULL;
+            delete temp;
+        }
+    }
+
+    void deleteAtTail()
+    {
+        Node *prev = head;
+        Node *temp = tail;
+        if (head == NULL)
+        {
+            return;
+        }
+        else if (head == tail)
+        {
+            tail = head = NULL;
+            return;
+        }
+        else
+        {
+
+            while (prev->next != tail)
+            {
+                prev = prev->next;
+            }
+
+            tail = prev;
+            tail->next = head;
+            cout << temp->data << endl;
+            temp->next = NULL;
+            delete temp;
+        }
+    }
+
+    void insertAtTail(int val)
+    {
+        Node *newNode = new Node(val);
+        if (head == NULL)
+        {
+            tail = head = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = newNode;
+        }
+        tail->next = head;
+        return;
     }
 };
 
@@ -92,6 +162,15 @@ int main()
     cl.insertAtHead(5);
     cl.insertAtHead(6);
     cl.insertAtHead(7);
+    cl.print();
+    cl.insertAtTail(4);
+    cl.insertAtTail(3);
+    cl.insertAtTail(12);
+    cl.print();
+    cl.deleteAtHead();
+    cl.deleteAtHead();
+    cl.deleteAtTail();
+    cl.deleteAtTail();
     cl.print();
     return 0;
 }
