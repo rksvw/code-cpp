4447210982e0c2efaa22386f56f665ed25786c38
rksvw <ritik.vw98@gmail.com>
2025-09-05 14:54:48 +0530

    some basic fixes on fibonacci & factorial

--- javascript-dsa/recursion.js
+++ javascript-dsa/recursion.js
@@ -1,21 +1,14 @@
 // WAP to find the factorial of N-th number
 const fact = (num, dp) => {
-  if (num === 1) {
-    return num;
-  }
-  if (num === 2) {
-    return num;
-  }
-
   return num * dp[num - 2];
 };
 
 // WAP to find the factorial of N times of N-th numbers
 
 const nOfNFactorial = (num) => {
-  const nFact = [];
+  const nFact = [1, 2];
 
-  for (let i = 1; i <= num; i++) {
+  for (let i = 3; i <= num; i++) {
     nFact.push(fact(i, nFact));
   }
 
@@ -26,14 +19,6 @@ const nOfNFactorial = (num) => {
 
 // WAP to find the fibonacci series of N-th number
 const fibo = (num, dp) => {
-  if (num === 0) {
-    return 0;
-  }
-
-  if (num === 1) {
-    return 1;
-  }
-
   return dp[num - 1] + dp[num - 2];
 };
 
@@ -51,3 +36,4 @@ const nOfNFibonacci = (num) => {
 };
 
 nOfNFibonacci(100);
+nOfNFactorial(100);
